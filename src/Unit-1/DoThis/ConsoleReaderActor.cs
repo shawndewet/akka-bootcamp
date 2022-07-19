2363d4a3f2f8546fe5dab4e765409b6c27889181
Shawn de Wet <shawndewet@codewell.co.za>
2022-07-19 09:40:21 +0200

    Lesson 2: process based on message type

--- src/Unit-1/DoThis/ConsoleReaderActor.cs
+++ src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -10,6 +10,8 @@ namespace WinTail
     class ConsoleReaderActor : UntypedActor
     {
         public const string ExitCommand = "exit";
+        public const string StartCommand = "start";
+
         private IActorRef _consoleWriterActor;
 
         public ConsoleReaderActor(IActorRef consoleWriterActor)
@@ -19,22 +21,78 @@ namespace WinTail
 
         protected override void OnReceive(object message)
         {
+            if (message.Equals(StartCommand))
+            {
+                PrintInstructions();
+            }
+            else if (message is Messages.InputError)
+            {
+                _consoleWriterActor.Tell(message as Messages.InputError);
+            }
+
+            GetAndValidateInput();
+
+        }
+
+        #region Internal methods
+
+        private void PrintInstructions()
+        {
+            Console.WriteLine("Write whatever you want into the console!");
+            Console.Write("Some lines will appear as");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write(" red ");
+            Console.ResetColor();
+            Console.Write(" and others will appear as");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(" green! ");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
+        }
+
+        /// <summary>
+        /// Reads input from console, validates it, then signals appropriate response
+        /// </summary>
+        private void GetAndValidateInput()
+        {
             Console.WriteLine($"Thread {System.Threading.Thread.CurrentThread.ManagedThreadId} says Type Something:");
-            var read = Console.ReadLine();
-            if (!string.IsNullOrEmpty(read) && String.Equals(read, ExitCommand, StringComparison.OrdinalIgnoreCase))
+            var message = Console.ReadLine();
+            if (string.IsNullOrEmpty(message))
+            {
+                Self.Tell(new Messages.NullInputError("No input received."));
+            }
+            else if (String.Equals(message, ExitCommand, StringComparison.OrdinalIgnoreCase))
             {
                 // shut down the system (acquire handle to system via
                 // this actors context)
                 Context.System.Terminate();
-                return;
+                //return;
             }
-
-            // send input to the console writer to process and print
-            _consoleWriterActor.Tell(read);
-
-            // continue reading messages from the console
-            Self.Tell("continue");
+            else
+            {
+                var valid = IsValid(message);
+                if (valid)
+                {
+                    _consoleWriterActor.Tell(new Messages.InputSuccess("Thanks for valid message"));
+                    // continue reading messages from the console
+                    Self.Tell(new Messages.ContinueProcessing());
+                }
+                else
+                {
+                    Self.Tell(new Messages.ValidationError("Invalid: input was odd"));
+                }
+            }
         }
 
+        private bool IsValid(string message)
+        {
+            return message.Length % 2 == 0;
+        }
+
+
+
+        #endregion
     }
 }
