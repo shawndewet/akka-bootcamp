2c0a573099b64722c79c97a08f104cb6224b92b0
Shawn de Wet <shawndewet@codewell.co.za>
2022-08-03 12:17:13 +0200

    Lesson 4 Phase 1 done.

---
+++ src/Unit-1/DoThis/TailActor.cs
@@ -0,0 +1,105 @@
+﻿using Akka.Actor;
+using System;
+using System.IO;
+using System.Text;
+
+namespace WinTail
+{
+    public class TailActor : UntypedActor
+    {
+
+        #region Message types
+
+        /// <summary>
+        /// Signal that the file has changed, and we need to read the next line of the file
+        /// </summary>
+        public class FileWrite
+        {
+            public FileWrite(string fileName)
+            {
+                FileName = fileName;
+            }
+
+            public string FileName { get; }
+        }
+
+        /// <summary>
+        /// Signal that hte OS had an error accessing the file
+        /// </summary>
+        public class FileError
+        {
+            public FileError(string fileName, string reason)
+            {
+                FileName = fileName;
+                Reason = reason;
+            }
+
+            public string FileName { get; }
+            public string Reason { get; }
+        }
+
+        /// <summary>
+        /// Signal to thread the iniital contents of the file at actor startup
+        /// </summary>
+        public class InitialRead
+        {
+            public InitialRead(string fileName, string text)
+            {
+                FileName = fileName;
+                Text = text;
+            }
+
+            public string FileName { get; }
+            public string Text { get; }
+        }
+
+        #endregion
+
+        private readonly string _filePath;
+        private readonly IActorRef _reporterActor;
+        private readonly FileObserver _observer;
+        private readonly Stream _fileStream;
+        private readonly StreamReader _fileStreamReader;
+
+        public TailActor(string filePath, IActorRef reporterActor)
+        {
+            _filePath = filePath;
+            _reporterActor = reporterActor;
+
+            //start watching the file for changes
+            _observer = new FileObserver(Self, Path.GetFullPath(_filePath));
+            _observer.Start();
+
+            // open the file stream with shared read/write permissions (so file can be written to elsewhere while open here)
+            _fileStream = new FileStream(Path.GetFullPath(_filePath), FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            _fileStreamReader = new StreamReader(_fileStream, Encoding.UTF8);
+
+            //read the initial contents of the file and send it to console as first msg
+            var text = _fileStreamReader.ReadToEnd();
+            Self.Tell(new InitialRead(_filePath, text));
+        }
+
+        protected override void OnReceive(object message)
+        {
+            if (message is FileWrite)
+            {
+                // move file cursor forward
+                // pull results from cursor to end of file and writ to output
+                // (this is assuming a log file type format that is append-only)
+                var text = _fileStreamReader.ReadToEnd();
+                if (!string.IsNullOrEmpty(text))
+                    _reporterActor.Tell(text);
+            }
+            else if (message is FileError)
+            {
+                var fe = message as FileError;
+                _reporterActor.Tell($"Tail error: {fe.Reason}");
+            }
+            else if (message is InitialRead)
+            {
+                var ir = message as InitialRead;
+                _reporterActor.Tell(ir.Text);
+            }
+        }
+    }
+}
---
+++ src/Unit-1/DoThis/TailCoordinatorActor.cs
@@ -0,0 +1,49 @@
+﻿using Akka.Actor;
+using System;
+
+namespace WinTail
+{
+    public class TailCoordinatorActor : UntypedActor
+    {
+        #region Message Types
+
+        public class StartTail
+        {
+            public StartTail(string filePath, IActorRef reporterActor)
+            {
+                FilePath = filePath;
+                ReporterActor = reporterActor;
+            }
+
+            public string FilePath { get; }
+            public IActorRef ReporterActor { get; }
+        }
+
+        public class StopTail
+        {
+            public StopTail(string filePath)
+            {
+                FilePath = filePath;
+
+            }
+
+            public string FilePath { get; }
+        }
+
+        #endregion
+
+        protected override void OnReceive(object message)
+        {
+            if (message is StartTail)
+            {
+                var msg = message as StartTail;
+
+                //here we are creating our first parent/child relationship.
+                // the TailActor instance created here is a chle of this instance of TailCoordinatorActor
+
+                Context.ActorOf(Props.Create(() => new TailActor(msg.FilePath, msg.ReporterActor)));
+
+            }
+        }
+    }
+}
